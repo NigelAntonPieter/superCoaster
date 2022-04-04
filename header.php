4000b3c16b590a38042653d9f3e15ee4c413514b
NigelAntonPieter <na.pieter@hotmail.nl>
2022-04-04 11:00:50 +0200

    css code voor de header

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <Header>
 	<div class="container">
 		<nav>
-			<img src="<?php echo $base_url; ?>/img/LOGO.jpg" alt="logo">
+			<img src="<?php echo $base_url; ?>/img/LOGO.jpg" alt="logo" class="logo">
 			<a href="<?php echo $base_url; ?>/index.php">Home</a> |
-			<a href=<?php echo $base_url; ?>">car.php">Karretjes</a> |
-			<a href="<?php echo $base_url; ?>"></a>
+			<a href="<?php echo $base_url; ?>/car.php">Karretjes</a> |
+			<a href="<?php echo $base_url; ?>/form.php">Contactformulier</a> 
 
 		</nav>
 	</div>
