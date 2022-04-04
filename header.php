e2bf0f6bfadc4159661a451cfb7b7213c9494120
NigelAntonPieter <na.pieter@hotmail.nl>
2022-04-04 10:56:37 +0200

    Header en Footer.php gemaakt en alvast aan de header begonnen

---
+++ header.php
@@ -0,0 +1,11 @@
+<Header>
+	<div class="container">
+		<nav>
+			<img src="<?php echo $base_url; ?>/img/LOGO.jpg" alt="logo">
+			<a href="<?php echo $base_url; ?>/index.php">Home</a> |
+			<a href=<?php echo $base_url; ?>">car.php">Karretjes</a> |
+			<a href="<?php echo $base_url; ?>"></a>
+
+		</nav>
+	</div>
+</Header>
