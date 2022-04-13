142340eb9499f40bd0ac04ba3e7394be0bc93b6b
NigelAntonPieter <na.pieter@hotmail.nl>
2022-04-13 16:32:03 +0200

    andere titel van gemaakt

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
   
     <div class="container">
-      <h1>WELKOM BESTE BEZOEKER</h1>
+      <h1>WELKOM BESTE BEZOEKER BIJ DE NIEUWE INOVATIE VAN SUPERCOASTER</h1>
       <img src="img/attractie achtergrond.jpg" alt="achtbaan">
      <p>Onze nieuwe achtbaantechniek heet MagnaDrive. Door middel van een aantal onderdelen die ons ontwikkelteam uitgevonden heeft, kunnen we een achtbaan produceren die tot 5-10% sneller kan gaan dan de snelste achtbaan tot nu toe, enkele graden scherpte kan toevoegen aan bochten, en ook nog langer meegaat dan achtbanen gemaakt van gebruikelijke onderdelen. Het mooiste is nog dat de onderdelen niet veel duurder zullen zijn dan onze concurrentie. Vanwege de scherpere bochten is in veel gevallen ook de achtbaan compacter te maken, wat ook weer ruimte vrijmaakt voor andere attracties.</p>
 
