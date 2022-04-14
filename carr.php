d73f01acd2fdfb4ad17f0e7711617bf60fbbbe12
jimbolol12 <D273640@edu.rocwb.nl>
2022-04-14 10:55:50 +0200

    Fix #13
    carr pagina aff

--- carr.php
+++ carr.php
@@ -3,14 +3,32 @@
 <head>
 	<title>SuperCoaster</title>
   <?php require_once 'head.php' ?> 
+  	<?php require_once 'header.php' ?> 
 </head>
 <body>
-	<?php require_once 'header.php' ?> 
-	<h1>meer over de rolercoster</h1>
-	<div class="carImg">
-		<img src="img/POR PLA-II & SAM-I plaatje8.jpg" alt="car">
+
+<!-- 	<h1>meer over de rolercoster</h1> -->
+<div class="foto1">
+	<div class="text1">
+		<h1>nieuw model</h1>
+		<p>Met onze MagnaCarts die ook speciaal werken op onze nieuw ontwikkelde MagnaAlloy <br>onderdelen, kan elk pretpark het chassis naar wens aanpassen. <br> Wij hebben een frame ontwikkeld  die wij zo klein<br> mogelijk hebben gehouden, zodat de rest van de kar helemaal<br> zelf door het team ontworpen en ontwikkeld kan worden</p>
 	</div>
-	
+	<img src="img/POR PLA-II & SAM-I plaatje8.jpg" alt="car">
+</div>
+<div class="foto2">
+		<div class="text2">
+		<h1>ontwerp</h1>
+		<p>Ook leveren wij de digitale bestanden waar deze<br> ontwerpen op gebaseerd kunnen worden, zodat het team zo snel <br> mogelijk aan de slag kan gaan unieke ontwerpen te creëren. <br> Omdat ook deze karretjes gebruik maken van MagnaAlloy, zijn <br> ze dus ook bestand tegen de extra G-krachten die deze techniek mogelijk maakt.</p>
+	</div>
+	<img src="img/POR PLA-II & SAM-I plaatje7.jpg" alt="car">
+</div>
+<div class="foto3">
+		<div class="text3">
+		<h1>achtbaantechniek</h1>
+		<p>Zo zijn onze producten dus breed inzetbaar, en zorgen wij <br>voor de beste vooruitgang in de achtbaantechniek <br>van het afgelopen decennia.</p>
+	</div>
+	<img src="img/POR PLA-II & SAM-I plaatje9.jpg" alt="car">
+</div>
 
 	
 
